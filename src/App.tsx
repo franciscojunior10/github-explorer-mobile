a93101f341735ede1ad934d07d2dddf2e230e448
Francisco Junior <franciscojn49@gmail.com>
2020-07-09 23:09:51 -0300

    feat: created project with react-native.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>hello world!</Text>
+    </View>
+  );
+};
+
+export default App;
