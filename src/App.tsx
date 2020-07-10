49fbd7ce407cf06649d6b5065d150a215f1bfda5
Francisco Junior <franciscojn49@gmail.com>
2020-07-10 00:47:38 -0300

    feat: added splash and icons drawable.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar, StyleSheet} from 'react-native';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>hello world!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#F0F0F5"/>
+      <View style={styles.container}>
+        <Text>hello world!</Text>
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F0F5',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
 
 export default App;
